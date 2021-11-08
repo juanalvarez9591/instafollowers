8f60b6c499c6668becf1b36df6e2f1b66090ce87
Juan Alvarez <juanalvarez9591@gmail.com>
2021-11-08 01:05:21 -0300

    extremadamente basado

--- popup.js
+++ popup.js
@@ -1,23 +1,59 @@
-let changeColor = document.getElementById("changeColor");
+let scanbtn = document.getElementById("scanbtn");
+let test = document.getElementById("test");
+let text = document.getElementById("text");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
 
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//chrome.storage.sync.get("color", ({ color }) => {
+//  changeColor.style.backgroundColor = color;
+//});
+
+scanbtn.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: scanPage,
   });
 });
 
+test.addEventListener("click", async () => {
+    chrome.storage.local.get("following", ({ following }) => {
+        text.innerText = following
+    })
+    
+});
+
+
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
-    const followersdiv = document.querySelector('[aria-label="Seguidores"]');
+function scanPage() {
+    const isFollowers = document.querySelector('[aria-label="Seguidores"]') ? true : false
+    const followerFocus = document.querySelector('[aria-label="Seguidores"]') ? document.querySelector('[aria-label="Seguidores"]') : document.querySelector('[aria-label="Seguidos"]')
+    
+    const max = document.getElementsByClassName("g47SY ")
 
-    const followers = Array.from(followersdiv.getElementsByTagName("a")).map(i => i.innerText)
-    console.log(followers)
+    const limit = document.querySelector('[aria-label="Seguidores"]') ? max[1].innerText : max[2].innerText + 2
+
+    let scrapedata = []
+
+    const loop = setInterval(() => {
+        const gordoguishe = document.getElementsByClassName("isgrP")[0];
+        gordoguishe.scrollTop = gordoguishe.scrollHeight - gordoguishe.clientHeight;
+        scrapedata = Array.from(followerFocus.getElementsByTagName("a")).map(i => i.innerText).filter(word => word.trim().length > 0)
+        console.log(scrapedata);
+        if (scrapedata.length == limit) {
+            clearInterval(loop);
+            if (isFollowers == true) {
+                const followers = scrapedata;
+                chrome.storage.local.set({ followers });
+            } else {
+                // This deletes the first two elements which are People and Hashtags
+                scrapedata.shift();
+                scrapedata.shift();
+                const following = scrapedata;
+                chrome.storage.local.set({ following });
+                console.log(following)
+            }
+        };
+    }, 1000)
 }
