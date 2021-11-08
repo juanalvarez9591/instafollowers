8c2a9bdadf47bdb7c22dfdb3ae1a779cb790d39e
Juan Alvarez <juanalvarez9591@gmail.com>
2021-11-07 22:37:54 -0300

    D

---
+++ popup.js
@@ -0,0 +1,23 @@
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+    const followersdiv = document.querySelector('[aria-label="Seguidores"]');
+
+    const followers = Array.from(followersdiv.getElementsByTagName("a")).map(i => i.innerText)
+    console.log(followers)
+}
