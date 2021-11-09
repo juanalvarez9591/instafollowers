3f395a949ed833e203e256394f63fd8c75d1fc67
Juan Alvarez <juanalvarez9591@gmail.com>
2021-11-09 00:32:37 -0300

    v 1.0

--- results.js
+++ results.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let div1 = document.getElementById("div1");
-    let div2 = document.getElementById("div2");
+    let nonfollowers = document.getElementById("nonfollowers");
+    let dnotfollowing = document.getElementById("notfollowing");
     let pfl = document.getElementById("followerslength");
     let pfl2 = document.getElementById("followinglength");
     let pnfl = document.getElementById("notfollowingbacklength");
@@ -13,8 +13,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const notfollowing = following.filter(i => !followers.includes(i))
 
     pfl.innerText += " "+followers.length
-    pfl2.innerText += following.length
-    pnfl.innerText += notfollowingback.length
-    pnfl2.innerText += notfollowing.length
+    pfl2.innerText += " "+following.length
+    pnfl.innerText += " "+notfollowingback.length
+    pnfl2.innerText += " " + notfollowing.length
+        
+        notfollowingback.map(i => {
+            let element = document.createElement("a")
+            element.innerText = i
+            element.href = "https://www.instagram.com/" + i + "/"
+            element.target = "_blank"
+            nonfollowers.append(element)
+        })
+        
+        notfollowing.map(i => {
+            let element = document.createElement("a")
+            element.innerText = i
+            element.href = "https://www.instagram.com/" + i + "/"
+            element.target = "_blank"
+            dnotfollowing.appendChild(element)
+        })        
+
+
     })
 });
